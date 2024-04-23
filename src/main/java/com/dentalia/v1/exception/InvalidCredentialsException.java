67e4198cb3c1e7ce0b1a37d206135ad7ac139c54
Manuellink1409 <manuelaft14@gmail.com>
2024-04-22 21:39:50 -0600

    Exceptions

---
+++ src/main/java/com/dentalia/v1/exception/InvalidCredentialsException.java
@@ -0,0 +1,15 @@
+package com.dentalia.v1.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException() {
+    }
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+    public InvalidCredentialsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/dentalia/v1/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.dentalia.v1.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
