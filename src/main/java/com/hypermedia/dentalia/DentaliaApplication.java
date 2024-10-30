4c2dedc4c9c798b9bae7da72be325290ef97ac30
Manuellink1409 <manuelaft14@gmail.com>
2024-10-30 12:21:28 -0600

    first commit

---
+++ src/main/java/com/hypermedia/dentalia/DentaliaApplication.java
@@ -0,0 +1,13 @@
+package com.hypermedia.dentalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypermedia/dentalia/DentaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypermedia.dentalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
